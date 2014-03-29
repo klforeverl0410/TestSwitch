89bed0637eb95f396bee12f0aa280a659a71deda
张兰 <zhanglan@zhanglantekiMacBook-Pro.local>
2014-03-29 15:04:38 +0800

    Initial Commit

---
+++ TestSwitch/TestSwitchAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TestSwitchAppDelegate.h
+//  TestSwitch
+//
+//  Created by 张兰 on 14-3-29.
+//  Copyright (c) 2014年 张兰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestSwitchAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestSwitch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestSwitch
+//
+//  Created by 张兰 on 14-3-29.
+//  Copyright (c) 2014年 张兰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TestSwitchAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TestSwitchAppDelegate class]));
+    }
+}
